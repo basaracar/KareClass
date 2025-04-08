9f2b338cea0bd3af3f4797efda8f0cf63a24f21c
basaracar <basaracaroglu@gmail.com>
2025-04-08 14:06:16 +0200

    Update connection strings in appsettings.json, modify assembly info version, and add .gitignore for sensitive files

--- obj/Debug/net9.0/KareClass.AssemblyInfo.cs
+++ obj/Debug/net9.0/KareClass.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KareClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2504331941e0e7f7dc8b6d027b6993bf19a7cdd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d1ee23dfba28ec2f94584f674cba922b581298")]
 [assembly: System.Reflection.AssemblyProductAttribute("KareClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KareClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
